98bdcfe02430fbec43c28a5e44ea4bd4c2fb3cfb
Makegood <makegood2you@gmail.com>
2022-04-05 19:21:40 +0300

    feat: init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+import { SharedComponentsVue2Sample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  SharedComponentsVue2Sample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <shared-components-vue2-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as SharedComponentsVue2Sample } from './shared-components-vue2-sample.vue';
